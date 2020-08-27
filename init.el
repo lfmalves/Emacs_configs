3edabd7cce1d0db42baea07957ba978ae4d02ee4
lfmalves <luis.projensino@gmail.com>
2020-08-27 10:19:29 -0300

    clean

--- init.el
+++ init.el
@@ -1,8 +1,5 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (require 'neotree)
@@ -21,15 +18,6 @@
 (add-hook 'elixir-mode-hook #'smartparens-mode)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(aggressive-indent smartparens neotree magit centaur-tabs elixir-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  '(package-selected-packages
+    '(aggressive-indent smartparens neotree magit centaur-tabs elixir-mode)))
+(custom-set-faces)
