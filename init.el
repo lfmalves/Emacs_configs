f6cf39801f0b510d248eec113a1dd8138dcd8483
lfmalves <luis.projensino@gmail.com>
2020-08-29 09:55:22 -0300

    projectile

--- init.el
+++ init.el
@@ -20,7 +20,20 @@
 (add-hook 'elixir-mode-hook
           (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
 (custom-set-variables
-  '(package-selected-packages
-    '(aggressive-indent smartparens neotree magit centaur-tabs elixir-mode)))
-(custom-set-faces)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(projectile alchemist aggressive-indent smartparens neotree magit centaur-tabs elixir-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
