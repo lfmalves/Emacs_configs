d0262824b1d6c91bb519ef7847e262abba86672c
Luis F M Alves <45541532+lfmalves@users.noreply.github.com>
2025-03-31 21:41:13 -0300

    faster boot init.el

--- init.el
+++ init.el
@@ -1,43 +1,72 @@
+;; Initialize package sources
 (require 'package)
+(setq package-enable-at-startup nil) ; Prevent double package loading
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(require 'neotree)
-(global-set-key (kbd "C-3") 'neotree-toggle)
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-(require 'centaur-tabs)
-(centaur-tabs-mode t)
-(global-set-key (kbd "C-1")  'centaur-tabs-backward)
-(global-set-key (kbd "C-2") 'centaur-tabs-forward)
-
-(global-aggressive-indent-mode 1)
-(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
-
+;; Load theme early for UI performance
 (load-theme 'cyberpunk t)
 
+;; Neotree
+(use-package neotree
+  :bind ("C-3" . neotree-toggle))
+
+;; Centaur Tabs
+(use-package centaur-tabs
+  :init
+  (setq centaur-tabs-style "bar"
+        centaur-tabs-set-icons t)
+  :config
+  (centaur-tabs-mode t)
+  :bind
+  (("C-1" . centaur-tabs-backward)
+   ("C-2" . centaur-tabs-forward)))
+
+;; Aggressive Indent
+(use-package aggressive-indent
+  :hook (prog-mode . global-aggressive-indent-mode)
+  :config
+  (add-to-list 'aggressive-indent-excluded-modes 'html-mode))
+
+;; Line Numbers
 (add-hook 'prog-mode-hook 'linum-mode)
 
-(add-hook 'elixir-mode-hook #'smartparens-mode)
+;; Smartparens for Elixir
+(use-package smartparens
+  :hook (elixir-mode . smartparens-mode))
 
+;; Auto-format on save for Elixir
 (add-hook 'elixir-mode-hook
-          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+          (lambda ()
+            (add-hook 'before-save-hook 'elixir-format nil t)))
 
-(add-hook 'after-init-hook 'global-company-mode)
+;; Company-mode (auto-completion)
+(use-package company
+  :hook (after-init . global-company-mode))
 
-(require 'projectile)
-(define-key projectile-mode-map (kbd "C-0") 'projectile-command-map)
-(projectile-mode +1)
+;; Projectile
+(use-package projectile
+  :init (setq projectile-keymap-prefix (kbd "C-0"))
+  :config (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("C-0" . projectile-command-map)))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(projectile alchemist aggressive-indent smartparens neotree magit centaur-tabs elixir-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Optional: Magit
+(use-package magit :defer t)
+
+;; Elixir mode and Alchemist
+(use-package elixir-mode :defer t)
+(use-package alchemist :defer t)
+
+;; Save selected packages
+(setq package-selected-packages
+      '(projectile alchemist aggressive-indent smartparens
+                   neotree magit centaur-tabs elixir-mode use-package))
+
